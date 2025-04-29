e7bf192bba5042dd057f5a2d5c29e101e3f71d89
apbrandom <apbrandom@gmail.com>
2025-04-29 21:07:26 +0200

    Initial Commit

---
+++ Grocery List/Grocery_ListApp.swift
@@ -0,0 +1,17 @@
+//
+//  Grocery_ListApp.swift
+//  Grocery List
+//
+//  Created by Vadim Vinogradov on 4/29/25.
+//
+
+import SwiftUI
+
+@main
+struct Grocery_ListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
