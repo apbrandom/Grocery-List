af517e1683fa680b25ced7230224b915c00cd25c
apbrandom <apbrandom@gmail.com>
2025-05-02 14:12:30 +0200

    Glocery List Preview update

--- Grocery List/ContentView.swift
+++ Grocery List/ContentView.swift
@@ -6,19 +6,56 @@
 //
 
 import SwiftUI
+import SwiftData
 
 struct ContentView: View {
+    @Environment(\.modelContext) private var modelContext
+    @Query private var item: [Item]
+    
     var body: some View {
-        VStack {
-            Image(systemName: "globe")
-                .imageScale(.large)
-                .foregroundStyle(.tint)
-            Text("Hello, world!")
+        NavigationStack {
+            List {
+                ForEach(item) { item in
+                    Text(item.title)
+                        .font(.title.weight(.light))
+                        .padding(.vertical, 2)
+                        .foregroundStyle(item.isCompleted == false ? Color.primary : Color.accentColor)
+                        .strikethrough(item.isCompleted)
+                        .italic(item.isCompleted)
+                }
+            } //: LIST
+            .navigationTitle("Grocery List")
+            .overlay {
+                if item.isEmpty {
+                    ContentUnavailableView("Empty Cart", systemImage: "cart.circle", description: Text("Add some items to your cart"))
+                }
+ 
+            }
         }
-        .padding()
     }
 }
 
-#Preview {
+#Preview("Sample Data") {
+    let sampleData: [Item] = [
+        Item(title: "Bakety & Bread", isCompleted: false),
+        Item(title: "Dairy", isCompleted: true),
+        Item(title: "Meat & Seafood", isCompleted: .random()),
+        Item(title: "Produce", isCompleted: .random()),
+        Item(title: "Pantry", isCompleted: .random()),
+    ]
+    
+    let container = try! ModelContainer(for: Item.self, configurations: ModelConfiguration(isStoredInMemoryOnly: true))
+    
+    for item in sampleData {
+        container.mainContext.insert(item)
+    }
+
+    return ContentView()
+        .modelContainer(container)
+        
+}
+
+#Preview("Empty List") {
     ContentView()
+        .modelContainer(for: Item.self, inMemory: true)
 }
